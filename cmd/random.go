8a89f3ada8c4e26debeeff6d0d986479d3c6b862
Avi Agarwal <coco.aviagarwal@gmail.com>
2023-06-12 22:40:53 +0530

    initial commit

---
+++ cmd/random.go
@@ -0,0 +1,99 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Get a random dad joke",
+	Long:  "This command fetches a random joke from icanhazdadjokes API.",
+	Run: func(cmd *cobra.Command, args []string) {
+		jokeTerm, _ := cmd.Flags().GetString("term")
+		if jokeTerm != "" {
+			getRandomJokeWithTerm(jokeTerm)
+		} else {
+			getRandomJoke()
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+	randomCmd.PersistentFlags().String("term", "", "A search term for a dad joke")
+}
+
+type Joke struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+type SearchResult struct {
+	Results    json.RawMessage `json:"results"`
+	SearchTerm string          `json:"search_term"`
+	Status     int             `json:"status"`
+	TotalJokes int             `json:"total_jokes"`
+}
+
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		log.Printf("Could not unmarshal response: %v", err)
+	}
+	fmt.Println(string(joke.Joke))
+}
+
+func getRandomJokeWithTerm(jokeTerm string) {
+	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeTerm)
+	responseBytes := getJokeData(url)
+	jokeListRaw := SearchResult{}
+	if err := json.Unmarshal(responseBytes, &jokeListRaw); err != nil {
+		log.Printf("Could not unmarshal response: %v", err)
+	}
+	jokes := []Joke{}
+	if err := json.Unmarshal(jokeListRaw.Results, &jokes); err != nil {
+		log.Printf("Could not unmarshal results: %v", err)
+	}
+	rndIndex := rand.Intn(len(jokes))
+	fmt.Println(jokes[rndIndex].Joke)
+}
+
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		baseAPI,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Could not create request: %v", err)
+
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "dad CLI")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Printf("Could not receive response: %v", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Could not read response body: %v", err)
+	}
+
+	return responseBytes
+}
