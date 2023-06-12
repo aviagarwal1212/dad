5f63e49f5450f2ff6187541bb7158c76070e3aff
Avi Agarwal <coco.aviagarwal@gmail.com>
2023-06-12 22:59:02 +0530

    implement cleaner exit in case of error

--- cmd/random.go
+++ cmd/random.go
@@ -47,52 +47,62 @@ type SearchResult struct {
 	TotalJokes int             `json:"total_jokes"`
 }
 
+
 func getRandomJoke() {
 	url := "https://icanhazdadjoke.com/"
 	responseBytes := getJokeData(url)
 	joke := Joke{}
 	if err := json.Unmarshal(responseBytes, &joke); err != nil {
-		log.Printf("Could not unmarshal response: %v", err)
+		log.Fatalf("Could not unmarshal response: %v", err)
 	}
 	fmt.Println(string(joke.Joke))
 }
 
+
 func getRandomJokeWithTerm(jokeTerm string) {
 	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeTerm)
-	responseBytes := getJokeData(url)
+	responseByte
+	s := getJokeData(url)
 	jokeListRaw := SearchResult{}
 	if err := json.Unmarshal(responseBytes, &jokeListRaw); err != nil {
-		log.Printf("Could not unmarshal response: %v", err)
+		log.Fatalf("Could not unmarshal response: %v", err)
+	}
+	
+	if jokeListRaw.TotalJokes == 0 {
+		log.Fatalf("No results found for the search term: %v", jokeTerm)
 	}
 	jokes := []Joke{}
 	if err := json.Unmarshal(jokeListRaw.Results, &jokes); err != nil {
-		log.Printf("Could not unmarshal results: %v", err)
+		log.Fatalf("Could not unmarshal results: %v", err)
 	}
 	rndIndex := rand.Intn(len(jokes))
 	fmt.Println(jokes[rndIndex].Joke)
 }
 
 func getJokeData(baseAPI string) []byte {
-	request, err := http.NewRequest(
+	reques
+	t, err := http.NewRequest(
 		http.MethodGet,
 		baseAPI,
 		nil,
 	)
 	if err != nil {
-		log.Printf("Could not create request: %v", err)
+		log.Fatalf("Could not create request: %v", err)
 
 	}
+	
 	request.Header.Add("Accept", "application/json")
 	request.Header.Add("User-Agent", "dad CLI")
 
 	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		log.Printf("Could not receive response: %v", err)
+	if err != nil 
+	{
+		log.Fatalf("Could not receive response: %v", err)
 	}
 
 	responseBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Printf("Could not read response body: %v", err)
+		log.Fatalf("Could not read response body: %v", err)
 	}
 
 	return responseBytes
